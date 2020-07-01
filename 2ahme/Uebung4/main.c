5443fa9de8794d315f562aefe2e026107ffd826f
Jan Pommer <pomjam18@htl-kaindorf.at>
2020-07-01 20:33:39 +0200

    AIIT 26.6.2020

--- 2ahme/Uebung4/main.c
+++ 2ahme/Uebung4/main.c
@@ -7,7 +7,7 @@ int v [10][10];
     
  printf("Übung 4: 1mal1\n");
  
- for (int i = 1; i <= 10; i++) {
+ for (int i = 1; i < 10; i++) {
      for (int j = 0; j < 10; j++) {
       v[i][j] = j * i;   
      }
@@ -15,15 +15,18 @@ int v [10][10];
    
  printf("Dezimalsystem:\n");
  printf("    |");
- for (int i = 1; i <= 10; i++) {
+ for (int i = 1; i < 10; i++) {
      printf("%4d |", i);
  }
- 
+      printf("\n");
+
  for (int i = 1; i < 65; i++) {
      printf("-");
  }
- 
- for (int i = 1; i <= 10; i++) {
+       printf("\n");
+
+      
+ for (int i = 1; i < 10; i++) {
      printf("%3d |", i);
      for (int j = 1; j <= 10; j++) {
          printf("%4d |", v[i][j]);
